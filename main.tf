530f134a2bf3e3cbecde328fa56e72cbece52ba8
dhruv86 <50953752+dhruv86@users.noreply.github.com>
2024-12-04 22:18:27 +0530

    Added resource for cloud-datastore

--- main.tf
+++ main.tf
@@ -1,10 +1,21 @@
-resource "google_storage_bucket" "tfstate" {
-  name          = var.state_bucket
-  location      = var.region
-  project       = var.project_id
-  storage_class = "NEARLINE"
-  labels        = {
-    environment = "development"
-    created-by  = "terraform"
-  }
+# resource "google_storage_bucket" "tfstate" {
+#   name          = var.state_bucket
+#   location      = var.region
+#   project       = var.project_id
+#   storage_class = "NEARLINE"
+#   labels        = {
+#     environment = "development"
+#     created-by  = "terraform"
+#   }
+# }
+resource "cloud-datastore" "main" {
+
+  project    = var.project_id
+  name       = var.name
+  mode       = var.mode
+  location   = var.region
+  kind       = var.kind_name
+  indexes    = var.indexes
+  #depends_on = [time_sleep.wait_120_seconds]
+   
 }
--- variables.tf
+++ variables.tf
@@ -1,6 +1,10 @@
 variable "region" {
   type    = string
   default = "us-east1"
+  validation {
+    condition     = contains(["us-east1", "us-east4", "us-east5"], var.region)
+    error_message = "Valid values for var: region are (us-east1, us-east4, us-east5)."
+  }
 }
 
 variable "project_id" {
@@ -8,7 +12,48 @@ variable "project_id" {
   default = "DataStoreTest"
 }
 
+variable "mode" {
+  type        = string
+  default     = "Datastore Mode"
+  description = "The mode in which to created the datastore, i.e. Native or Datastore mode"
+}
+
 variable "state_bucket" {
   type    = string
   default = "tf_state_bucket_dhrj_1986"
 }
+
+variable "name" {
+  type        = string
+  description = "Name of the Datastore"
+  default     = "main_dhrj"
+}
+
+
+variable "kind_name" {
+  type    = string
+  default = "task"
+}
+
+variable "indexes" {
+  type    = string
+  default = <<EOF
+indexes:
+
+- kind: Task
+  ancestor: no
+  properties:
+  - name: done
+  - name: priority
+    direction: desc
+
+- kind: Task
+  properties:
+  - name: collaborators
+    direction: asc
+  - name: created
+    direction: desc
+EOF
+
+}
+
